f95733ede29428279dd568abe573d07c689517d6
Eduardo Lezama <elezama625@gmail.com>
2025-04-02 16:10:29 -0600

    ultimo

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const routes = require('./src/routes');
 const { init } = require('./src/servicelocator/composer')
 const port = 4000;
 
 routes(app);
+
+
+app.use(cors(
+    {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+    }
+));
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port }`);
